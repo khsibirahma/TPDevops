89c0d2622214be59718a0affe34fb01cede03f91
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-01-29 11:24:16 +0100

    nettoyage fichier navbar.js

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,14 +1,6 @@
-/*window.addEventListener('scroll', function () {
-    console.log(document.documentElement.scrollTop);
-});*/
-
 let taille1 = window.innerWidth;
 let taille2 = document.body.clientWidth;
 let taille3 = document.documentElement.clientWidth;
-
-/*console.log(taille1);
-console.log(taille2);
-console.log(taille3);*/
 
 //Si l'ecran > 800px
 if (taille1 > 800 || taille2 > 800 || taille3 > 800)
@@ -17,12 +9,9 @@ if (taille1 > 800 || taille2 > 800 || taille3 > 800)
     {
         let el = document.querySelector("#header");
         let logo = document.querySelector("#logo");
-        //let menu = document.querySelector(".menu");
-        //console.log("height ="+el.offsetHeight);
 
         if (document.documentElement.scrollTop > el.offsetHeight || document.body.scrollTop > el.offsetHeight)
         {
-            //console.log("superieur");
             el.style.position = "fixed";
             el.style.height = "6vh";
             el.style.padding = "0% 5%";
@@ -30,7 +19,6 @@ if (taille1 > 800 || taille2 > 800 || taille3 > 800)
         }
         else
         {
-            //console.log("inferieur");
             el.style.position = "relative";
             el.style.height = "14vh";
             el.style.padding = "1% 5%";
