ce1eec3a14ce440baeffaebdd98519388c729b00
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-01-29 11:14:59 +0100

    Fix page acceuil + navbar

--- JS/navbar.js
+++ JS/navbar.js
@@ -13,23 +13,27 @@ console.log(taille3);*/
 //Si l'ecran > 800px
 if (taille1 > 800 || taille2 > 800 || taille3 > 800)
 {
-    window.onscroll = function () {
+    window.onscroll = function () 
+    {
         let el = document.querySelector("#header");
         //let menu = document.querySelector(".menu");
         //console.log("height ="+el.offsetHeight);
 
-        if (document.documentElement.scrollTop > el.offsetHeight || document.body.scrollTop > el.offsetHeight){
+        if (document.documentElement.scrollTop > el.offsetHeight || document.body.scrollTop > el.offsetHeight)
+        {
             //console.log("superieur");
             el.style.position = "fixed";
             el.style.height = "6vh";
             el.style.padding = "0% 5%";
         }
-        else{
+        else
+        {
             //console.log("inferieur");
             el.style.position = "relative";
             el.style.height = "14vh";
             el.style.padding = "1% 5%";
         }
     };
-
 }
+
+
